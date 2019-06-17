cff7ee641aa90454cb67852401aca9d18dd8c03b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-17 18:28:45 +0800

    add json server

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'build', 'db.json'));
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, 'build', 'assets'),
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running at http://localhost:${PORT}`);
+});
